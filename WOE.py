0c25ca09efa60cad49126698e2d24ac6d67bd269
Alexis <alexis.drake5721@gmail.com>
2025-01-08 14:08:40 -0800

    First commit

---
+++ WOE.py
@@ -0,0 +1,24 @@
+"""" Words of Encouragement"""
+
+no_nonsense = {1: "Hurry up",
+         2: "My grandma can work faster than you",
+         3: "Don't be a loser",
+         4: "Stop wasting time",
+         5: "Quit wasting my time",
+         6: "Get to work",
+         7: "Why are you still reading this?",
+         8: "I would already be done by now",
+         9: "Don't suck",
+         10: "You don't need a snack before you start working"}
+
+nonsense = {1: "You're doing great",
+            2: "Keep it up",
+            3: "I bet you can finish this in less than 10 minutes",
+            4: "Keep up the good work",
+            5: "You're smarter than a fifth grader",
+            6: "Wish I was as cool as you are",
+            7: "You're a genius",
+            8: "Your code will have NO errors",
+            9: "A+ for starting tomorrow!.... Just kidding get to work!",
+            10: "I wish I could debug as well as you"}
+
