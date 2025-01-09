c1b97398e143a989ecb29db97115205c4367449a
Alexis <alexis.drake5721@gmail.com>
2025-01-09 13:57:13 -0800

    First Draft of program

---
+++ dictionaries.py
@@ -0,0 +1,111 @@
+
+# Words of Encouragement
+no_nonsense = {
+    1: "Hurry up",
+    2: "My grandma can work faster than you",
+    3: "Don't be a loser",
+    4: "Stop wasting time",
+    5: "Quit wasting my time",
+    6: "Get to work",
+    7: "Why are you still reading this?",
+    8: "I would already be done by now",
+    9: "Don't suck",
+    10: "You don't need a snack before you start working"}
+
+nonsense = {
+    1: "You're doing great",
+    2: "Keep it up",
+    3: "I bet you can finish this in less than 10 minutes",
+    4: "Keep up the good work",
+    5: "You're smarter than a fifth grader",
+    6: "Wish I was as cool as you are",
+    7: "You're a genius",
+    8: "Your code will have NO errors",
+    9: "A+ for starting tomorrow!.... Just kidding get to work!",
+    10: "I wish I could debug as well as you"}
+
+
+# List of projects
+machine_learning = {
+    1: "Detecting Spam Emails",
+    2: "Movie Recommendation based on emotion",
+    3: "Zillow Home Value (Zestimate) Prediction",
+    4: "Bitcoin Price Prediction",
+    5: "Count number of faces",
+    6: "Text Detection and Extraction",
+    7: "Fake News Detection",
+    8: "Hate Speech Detection",
+    9: "Autocorrect",
+    10: "Restaurant Review Analysis"
+}
+
+games = {
+    1: "Hangman",
+    2: "Battleship",
+    3: "Snake",
+    4: "Tower Defense",
+    5: "Pokemon Inspired",
+    6: "Hole.io",
+    7: "Aeroblasters",
+    8: "Text based story game",
+    9: "Memory Puzzle",
+    10: "Tetris"
+}
+
+data_science = {
+    1: "Interactive Weather Analysis",
+    2: "Crawling Restaurant",
+    3: "Premier League Twitter Activity",
+    4: "Football World Cup Analytics",
+    5: "Supply Chain Control Tower",
+    6: "Passive Income Analysis",
+    7: "Can AI be a data scientists?",
+    8: "Live Stock Market Visualization",
+    9: "Crawl Nintendo Game Reviews",
+    10: "Sankey Diagrams",
+}
+
+# Pycharm Auto filled the last 6 Good luck lol
+numpy_projects = {
+    1: "ASCII Art Animation",
+    2: "Gravitational Simulation",
+    3: "Implementation of Polynomial Regression",
+    4: "Implementation of Logistic Regression",
+    5: "Implementation of K-Nearest Neighbors",
+    6: "Implementation of Decision Tree",
+    7: "Implementation of Random Forest",
+    8: "Implementation of Support Vector Machine",
+    9: "Implementation of Naive Bayes",
+    10: "Implementation of K-Means Clustering"
+}
+
+django_projects = {
+    1: "Registration and Login System",
+    2: "Calculator Application",
+    3: "Resume Builder",
+    4: "Recipe Meal Planner",
+    5: "Blog Application",
+    6: "Photo Gallery Application",
+    7: "Todo List",
+    8: "E-commerce Website",
+    9: "Wikipedia Search Application",
+    10: "Quiz Application"
+}
+
+flask_projects = {
+    1: "Background Remover",
+    2: "Table Extraction",
+    3: "Doctor-Patient Appointment System",
+    4: "Portfolio Website",
+    5: "Healthcure - All in one medical solution",
+    6: "Words and Paragraph counter",
+    7: "Youtube Comments Extractor and Sentiment Analysis",
+    8: "URL Shortener",
+    9: "Flight Price Prediction",
+    10: "Weather Application"
+}
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,205 @@
-git add
+from tkinter import *
+import random as rn
+from dictionaries import *
+
+
+class MyGui:
+
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Mini Boss")
+        self.root.geometry("750x700")
+        self.root.config(background="#637074")
+
+        # Page Title
+        self.title_label = Label(
+            self.root,
+            text="Mini Boss",
+            bg="#637074",
+            font=("Arial", 34, "bold"),
+            fg="#f9f8f8"
+        )
+        self.title_label.pack(pady=5)
+
+        self.subtitle_label = Label(
+            self.root,
+            text="Welcome to Mini Boss, can't decide what to do? Let me decide for you!",
+            bg="#637074",
+            font=("Arial", 15),
+            fg="#f9f8f8"
+        )
+        self.subtitle_label.pack(pady=10)
+
+        self.space_label = Label(
+            self.root,
+            text="",
+            bg="#637074"
+        )
+        self.space_label.pack(pady=10)
+
+        # Project Buttons
+
+        # Displays Project
+        self.result_label = Label(
+            self.root,
+            text="Click a button to see a random project",
+            bg="#637074",
+            font=("Arial", 14),
+            fg="#ffffa7"
+        )
+        self.result_label.pack(pady=10)
+
+        # Project Buttons
+        self.button_to_category = {
+            "Machine Learning": machine_learning,
+            "Games": games,
+            "Data Science": data_science,
+            "Numpy": numpy_projects,
+            "Django": django_projects,
+            "Flask": flask_projects,
+        }
+
+        self.buttonframe = Frame(self.root, bg="#637074")
+        self.buttonframe.pack(side=TOP, pady=20)
+
+        button_names = list(self.button_to_category.keys())
+
+        button_positions = [
+            (0,0), (0,1), (0,2),
+            (1,0), (1,1), (1,2),
+        ]
+
+        # 6 Buttons
+        for i, name in enumerate(button_names):
+            button = Button(
+                self.buttonframe,
+                text=name,
+                command=lambda category=name: self.show_random_projects(category),
+                bg="#aacfe4",
+                width=20
+            )
+            button.grid(row=button_positions[i][0], column=button_positions[i][1], padx=10, pady=10)
+
+
+        # Time Frame
+
+        # Displays Time
+        self.time_label = Label(
+            self.root,
+            text="Do you know how to do this?",
+            bg="#637074",
+            font=("Arial", 14),
+            fg="#ffffa7"
+        )
+        self.time_label.pack(pady=10)
+
+        # Time Buttons
+        self.timeframe = Frame(self.root, bg="#637074")
+        self.timeframe.pack(pady=20)
+
+        time_positions = [(0,0), (0,1), (0,2)]
+
+        self.time_to_category = {
+            "I have NO idea": "30 days",
+            "In the middle": "15 days",
+            "Can do it with eyes closed": "3 days",
+        }
+
+        time_names = list(self.time_to_category.keys())
+
+        for i, name in enumerate(time_names):
+            button = Button(
+                self.timeframe,
+                text=name,
+                command=lambda category=name: self.show_time(category),
+                bg="#aacfe4",
+                width=20
+            )
+            button.grid(row=time_positions[i][0], column=time_positions[i][1], padx=10, pady=10)
+
+
+        # Words of encouragement
+
+        # Displays Words of Encouragement
+        self.encouragement_label = Label(
+            self.root,
+            text="Words of encouragement",
+            bg="#637074",
+            font=("Arial", 14),
+            fg="#ffffa7"
+        )
+        self.encouragement_label.pack(pady=10)
+
+        # Buttons of encouragement
+        self.encouragement_frame = Frame(self.root, bg="#637074")
+        self.encouragement_frame.pack(pady=20)
+
+        words_positions = [(0,0), (0,1), (0,2)]
+        self.encouragement_to_category = {
+            "Expectation": nonsense,
+            "Reality": no_nonsense
+        }
+        encouragement_names = list(self.encouragement_to_category.keys())
+        
+        for i, name in enumerate(encouragement_names):
+            button = Button(
+                self.encouragement_frame,
+                text=name,
+                command=lambda category=name: self.show_encouragement(category),
+                bg="#aacfe4",
+                width=20
+            )
+            button.grid(row=words_positions[i][0], column=words_positions[i][1], padx=10, pady=10)
+
+
+        # Reset and quit
+        self.options_frame = Frame(self.root, bg="#637074")
+        self.options_frame.pack(pady=20)
+
+        self.resetButton = Button(
+            self.options_frame,
+            text="Reset",
+            command=self.reset,
+            bg="#e67e22",
+            width=10
+        )
+        self.resetButton.grid(row=0, column=0, padx=5, pady=5)
+
+        self.quitButton = Button(
+            self.options_frame,
+            text="Quit",
+            command=self.root.quit,
+            bg="#c0392b",
+            width=10
+        )
+        self.quitButton.grid(row=0, column=1, padx=5, pady=5)
+
+        self.root.mainloop()
+
+    def show_random_projects(self, category):
+        """Displays a random project from the given category"""
+
+        selected_category = self.button_to_category[category]
+        random_project = rn.choice(list(selected_category.values()))
+        self.result_label.config(text=f"{category} Project: {random_project}")
+
+    def show_time(self, category):
+        """Shows the amount of time given for the project"""
+
+        selected_category = self.time_to_category[category]
+        self.time_label.config(text=f"You have {selected_category} to complete the project")
+
+    def show_encouragement(self, category):
+        selected_category = self.encouragement_to_category[category]
+        random_encouragement = rn.choice(list(selected_category.values()))
+        self.encouragement_label.config(text=f"{category} words: {random_encouragement}")
+
+    def reset(self):
+        self.result_label.config(text="Click a button to see a random project")
+        self.time_label.config(text="Do you know how to do this?")
+        self.encouragement_label.config(text="Words of encouragement")
+
+
+
+MyGui()
+
